5ee2cc4679d51bc951fbd81ddfb118b549c182dd
DevFigueiredo <miranda.f@aluno.ifsp.edu.br>
2020-05-31 11:48:00 -0500

    Criado a rota de Republica

---
+++ mobile/src/pages/Republic/index.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import {Text, View,ScrollView, ImageBackground, TouchableHighlight } from 'react-native';
+import styles from './style';
+
+
+export default function Republic( {navigation} ) {
+
+  return (
+<View>
+   <Text>Bem vindo a República XXXX</Text>
+</View>
+  );
+
+}
