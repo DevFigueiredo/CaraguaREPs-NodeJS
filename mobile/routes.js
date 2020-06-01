f0f0e30051b827ff1c37466d8b4d0b3086ee1ecb
DevFigueiredo <miranda.f@aluno.ifsp.edu.br>
2020-05-31 20:00:34 -0500

    Organizado

--- mobile/routes.js
+++ mobile/routes.js
@@ -15,18 +15,12 @@ const App = createStackNavigator();
 export default function Routes() {
   return (
     <NavigationContainer initialRouteName="Login">
-    <AppStack.Navigator screenOptions={{
-      headerShown: true,
-   }} 
-   
-  mode="modal"
-   >
+    <AppStack.Navigator screenOptions={{ headerShown: true, }} mode="modal" >
     
     <AppStack.Screen name="Login" component={Login} options={{headerShown:false}} />
     <AppStack.Screen name="Home" component={Principal} options={{headerShown:false}} />
     <App.Screen name="Message" component={Message} />
     <App.Screen name="Republic" component={Republic} options={{headerTitle: false, headerTransparent: true}} />
- 
     </AppStack.Navigator>
     </NavigationContainer>
     
